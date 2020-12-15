67e2af9d8920a8fbbb85a6df62cbbd6c34ba48ca
Rickey Dong <rickeyd2004@gmail.com>
2020-12-15 13:15:01 -0500

    created bubbleSort algorithm, tested it with [6,1,2,3,4,5] and it works, will test with more

---
+++ Sorts.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class Sorts
+{
+  public static void bubbleSort(int[] data)
+  {
+    //[2,3,4,5,1]
+    int temporary = 0;
+    System.out.println(Arrays.toString(data));
+    for (int i = data.length; i > 0; i--)
+    {
+      for (int j = 1; j < i; j++)
+      {
+        if (data[j] < data[j-1])
+        {
+          temporary = data[j]; //1
+          data[j] = data[j-1]; //[6,6]
+          data[j-1] = temporary; //[1,6]
+          System.out.println(Arrays.toString(data));
+        }
+      }
+    }
+  }
+}
---
+++ SortsTester.java
@@ -0,0 +1,8 @@
+public class SortsTester
+{
+  public static void main(String[] args)
+  {
+    int[] ourData = {6,1,2,3,4,5};
+    Sorts.bubbleSort(ourData);
+  }
+}
