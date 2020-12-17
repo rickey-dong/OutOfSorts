514c03fb58fa6bddfa5a37b6c5bd901e893e51d5
Rickey Dong <rickeyd2004@gmail.com>
2020-12-16 20:55:45 -0500

    deleted or commented out print statements

--- Sorts.java
+++ Sorts.java
@@ -5,7 +5,7 @@ public class Sorts
   {
     //[2,3,4,5,1]
     int temporary = 0;
-    System.out.println(Arrays.toString(data)); //DELETE THIS AFTER YOU ARE DONE
+    //System.out.println(Arrays.toString(data)); //DELETE THIS AFTER YOU ARE DONE
     for (int i = data.length; i > 0; i--)
     {
       for (int j = 1; j < i; j++)
@@ -15,7 +15,7 @@ public class Sorts
           temporary = data[j]; //1
           data[j] = data[j-1]; //[6,6]
           data[j-1] = temporary; //[1,6]
-          System.out.println(Arrays.toString(data)); //DELETE THIS AFTER YOU ARE DONE
+          //System.out.println(Arrays.toString(data)); //DELETE THIS AFTER YOU ARE DONE
         }
       }
     }
